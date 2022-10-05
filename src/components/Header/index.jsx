8da346c8ff9ba9e5e98175697e2864050acb986d
Dev Luis <lugamercrurm@gmail.com>
2022-10-05 11:06:05 -0300

    Adicionando Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="title-conteiner">
+                <h1>
+                    MathL
+                </h1>
+            </div>
+
+        </header>
+    );
+}
